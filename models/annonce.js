5fe1590d6c16913730f02d5910277cf1708812fa
mc152872 <calvin.mariette@etu.univ-lehavre.fr>
2020-09-28 13:59:44 +0200

    add models Mongoose

---
+++ models/annonce.js
@@ -0,0 +1,27 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var AnnonceSchema = new Schema({
+    titre: String,
+    type: {
+        type: String,
+        enum: ["Vente", "Location"]
+    },
+    statutPublication: {
+        type: String,
+        enum: ["Publié", "NonPublié"]
+    },
+    statutBien: {
+        type: String,
+        enum: ["Disponible", "Louée", "Vendu"]
+    },
+    description: String,
+    prix: Number,
+    disponibilite: Date,
+    photos:{
+        type: String
+    }
+});
+
+module.exports = mongoose.model('Annonce', AnnonceSchema);
+
